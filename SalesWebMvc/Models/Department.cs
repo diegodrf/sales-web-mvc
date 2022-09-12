72b42e838b906e14feb53ca1c52ab04607fcfdda
Diego Faria <diego.rdfaria@gmail.com>
2022-09-11 22:42:01 -0300

    Adicionando Departments MVC CRUD

---
+++ SalesWebMvc/Models/Department.cs
@@ -0,0 +1,7 @@
+﻿namespace SalesWebMvc.Models;
+
+public class Department
+{
+    public int Id { get; set; } = new Random().Next(1, int.MaxValue);
+    public string Name { get; set; } = string.Empty;
+}
