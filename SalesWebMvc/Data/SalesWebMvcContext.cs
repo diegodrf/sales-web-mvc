30e765e21bfe588a0823feadcc856d628392cd0e
Diego Faria <diego.rdfaria@gmail.com>
2022-09-12 23:22:00 -0300

    Refatorando

--- SalesWebMvc/Data/SalesWebMvcContext.cs
+++ SalesWebMvc/Data/SalesWebMvcContext.cs
@@ -11,9 +11,7 @@ namespace SalesWebMvc.Data
     public class SalesWebMvcContext : DbContext
     {
         public SalesWebMvcContext (DbContextOptions<SalesWebMvcContext> options)
-            : base(options)
-        {
-        }
+            : base(options) {}
         public DbSet<Department> Department { get; set; } = default!;
         public DbSet<Seller> Seller { get; set; } = default!;
         public DbSet<SalesRecord> SalesRecord { get; set; } = default!;
